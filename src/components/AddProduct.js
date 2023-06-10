05738e30141488d48367ccc9a2fe50dd9d6009b6
mostofa-s-cse <mostofa.s.cse@gmail.com>
2023-06-10 16:20:31 +0600

    addproduct done

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -1,8 +1,41 @@
-function AddProduct()
-{
+import { useState } from "react";
+
+function AddProduct(){
+const [name,setName] = useState("");
+const [price,setPrice] = useState("");
+const [description,setDescription] = useState("");
+const [image,setImage] = useState("");
+
+async function AddProduct(){
+    const formData = new FormData();
+    formData.append('image',image);
+    formData.append('name',name);
+    formData.append('price',price);
+    formData.append('description',description);
+
+    let result = await fetch("http://localhost:8000/api/addproduct",{
+        method:'POST',
+        // body:JSON.stringify(formData),
+        body:formData,
+    });
+    alert("data has been saved")
+
+
+}
     return (
-        <div>
-            <h1>AddProduct</h1>
+        <div className="col-md-6 m-auto text-center">
+            <div className="mt-5 shadow p-5">
+            <h1 className="mb-5">Add Product</h1>
+                <input type="text" value={name} onChange={(e)=>setName(e.target.value)} className="form-control" placeholder="Name" />
+                <br/>
+                <input type="text" value={price} onChange={(e)=>setPrice(e.target.value)} className="form-control" placeholder="price" />
+                <br/>
+                <input type="file" onChange={(e)=>setImage(e.target.files[0])} className="form-control" />
+                <br/>
+                <textarea type="text" value={description} onChange={(e)=>setDescription(e.target.value)} className="form-control" placeholder="description" />
+                <br/>
+                <button onClick={AddProduct} className="btn btn-primary">Sign Up</button>
+            </div>
         </div>
     )
 }
